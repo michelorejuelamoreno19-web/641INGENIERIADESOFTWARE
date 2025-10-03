7219991b8599b00b2a040a05f59e01809106b5d2
MichelOrejuela19 <michelorejuelamoreno19@gmail.com>
2025-10-03 02:27:47 +0000

    Creacion de pasos builder

---
+++ PatronBuilder/Converter.java
@@ -0,0 +1,13 @@
+public class converter { 
+    private String document; 
+
+    public void makeLine(){ 
+    } 
+    public void makeParagraph(){
+    }
+    public void makeTable(){ 
+    } 
+    public String getDocument(){
+     return document; 
+    } 
+}
